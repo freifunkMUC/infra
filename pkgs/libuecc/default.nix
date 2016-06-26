ace79e0f420714b9733750ccc38aba4ecb2d7cb2
Franz Pletz <fpletz@fnordicwalking.de>
2016-06-26 02:44:10 +0200

    libuecc: 6 -> 7

--- pkgs/libuecc/default.nix
+++ pkgs/libuecc/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchgit, cmake }:
 
 stdenv.mkDerivation rec {
-  version = "6";
+  version = "7";
   name = "libuecc-${version}";
 
   src = fetchgit {
     url = "git://git.universe-factory.net/libuecc";
     rev = "refs/tags/v${version}";
-    sha256 = "1rrgq6ld8v0n51637g9ggf7sfgzzjpbr1i3xc3pb69wyaws6gg7x";
+    sha256 = "1sm05aql75sh13ykgsv3ns4x4zzw9lvzid6misd22gfgf6r9n5fs";
   };
 
   buildInputs = [ cmake ];
